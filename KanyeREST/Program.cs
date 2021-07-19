685eb3adb4acb16913d0303b52ab9e8b2d82866c
Huynhdavid888 <huynhdavid1245@gmail.com>
2021-07-18 22:04:32 -0700

    initial commit

---
+++ KanyeREST/Program.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Net.Http;
+
+namespace KanyeREST
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("-------------------");
+                QuoteGenerator.KanyeQuote();
+                QuoteGenerator.RonQuote();
+            }
+
+
+
+
+        }
+    }
+}
